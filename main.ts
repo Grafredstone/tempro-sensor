6b9f8553ed501e93be8f4e12a5a136025c368a01
Grafrestone <80029985+Grafredstone@users.noreply.github.com>
2025-01-17 17:18:21 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.showString("" + Salt + Funkgruppe)
+})
+let Salt = 0
+let Funkgruppe = 0
 radio.setGroup(216)
-let Funkgruppe = randint(0, 255)
-let Salt = randint(0, 65536) * randint(0, 65536)
+Funkgruppe = randint(0, 255)
+Salt = randint(0, 65536) * randint(0, 65536)
 radio.sendValue(convertToText(Funkgruppe), Salt)
 radio.setGroup(Funkgruppe)
 DHT11.setPin(DigitalPin.P0)
