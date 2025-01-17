82f298472d20a709ddb8b8c492c441ce5680021a
Grafrestone <80029985+Grafredstone@users.noreply.github.com>
2025-01-17 17:00:57 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,7 @@ let Salt = randint(0, 65536) * randint(0, 65536)
 radio.sendValue(convertToText(Funkgruppe), Salt)
 radio.setGroup(Funkgruppe)
 DHT11.setPin(DigitalPin.P0)
-basic.showLeds(`
-    . . # # .
-    . # . . .
-    . . # . .
-    . . . # .
-    . # # . .
-    `)
+basic.showString("S")
 basic.forever(function () {
     radio.sendNumber(DHT11.temperature() * Salt)
     basic.setLedColor(0x00ffff)
